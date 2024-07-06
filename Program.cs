d0c48ce43af7a6ef46b44cc0d321cc24c2aed2df
mecho420 <ahmedhasanovahmedcho@gmail.com>
2024-07-06 12:14:39 +0300

    Added RealData, file path, book list and the program can now read non english symbols.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,28 @@ namespace LibraryRealProject
 {
     internal class Program
     {
+        private string filePath = "BooksList.txt";
+        List<Books> bookList = new List<Books>();
+
         static void Main(string[] args)
         {
+            // Да може да чете кирилица
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+        }
 
+        static void ReadData(string filePath)
+        {
+            StreamReader reader = new StreamReader(filePath, Encoding.Unicode);
+            using (reader)
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] bookInfo = line.Split(',');
+                    Books book = new Books();
+                }
+            }
         }
     }
 }
